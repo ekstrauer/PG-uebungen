19287b606e415047462d1e6eb45450ad7f93a0aa
Martin Dizdar <martin.dizdar02@gmail.com>
2025-06-04 16:55:00 +0200

    Übung1: Prä/Post-Inkrement

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    int a1 = 4;
+    int a2 = 4;
+    int b1 = 0;
+    int b2 = 0;
+
+    // Prä-Inkrement
+    b1 = ++a1;
+    cout << "Prä-Inkrement:" << endl;
+    cout << "b1 = " << b1 << ", a1 = " << a1 << endl;
+
+    // Post-Inkrement
+    b2 = a2++;
+    cout << "Post-Inkrement:" << endl;
+    cout << "b2 = " << b2 << ", a2 = " << a2 << endl;
+
     return 0;
 }
